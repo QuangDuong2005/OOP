a6b9ec0863b0072eaabd4a674a9dc736b2c2bb6d
QuangDuong2005 <85088667+QuangDuong2005@users.noreply.github.com>
2026-01-11 14:46:58 +0700

    Delete OOP directory

--- OOP/demo/src/main/java/expection/InvalidDateException.java
+++
@@ -1,7 +0,0 @@
-package expection;
-// Ngoại lệ cho ngày không hợp lệ
-public class InvalidDateException extends Exception {
-    public InvalidDateException(String message) {
-        super(message);
-    }   
-}
--- OOP/demo/src/main/java/expection/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package expection;
-// Ngoại lệ cho mật khẩu không hợp lệ
-public class InvalidPasswordException extends Exception {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
